128397ce39d9497f024a312c8e21f9301b3f9ba8
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-01-24 17:19:58 +0200

    refactor(flake.nix): switch back to nixos-unstable
    Although `master` is more advanced than `unstable`, which was the initial reason for not using `unstable`, a lot of times `home-manager` was being broken for `darwin`, thus making this branch unreliable for this config

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS & Darwin System Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     flake-utils.url = "github:numtide/flake-utils";
