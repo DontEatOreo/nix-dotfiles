dc59c8b29fbc6bc4e77c51ee8a3f17f2bd10e659
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-03-20 19:56:12 +0200

    feat(vscode): add proper C# Support
    The C# DevKit (csdevkit) extension and .NET Install Tool (vscode-dotnet-runtime) require some extra patching to work under Nix

--- home-manager/programs/vscode/extensions.nix
+++ home-manager/programs/vscode/extensions.nix
@@ -22,7 +22,68 @@ in
 
       ## C#
       ms-dotnettools.csharp
-      ms-dotnettools.csdevkit
+      (ms-dotnettools.csdevkit.overrideAttrs (_: {
+        sourceRoot = "extension";
+        postPatch = with pkgs; ''
+          declare patchCommand
+          declare add_rpath_command
+          if [[ "$(uname)" == "Darwin" ]]; then
+            patchCommand="install_name_tool"
+            add_rpath_command="-add_rpath"
+          else
+            patchCommand="patchelf"
+            add_rpath_command="--set-rpath"
+          fi
+
+          patchelf_add_icu_as_needed() {
+            declare elf="''${1?}"
+            declare icu_major_v="${lib.head (lib.splitVersion (lib.getVersion icu.name))}"
+            for icu_lib in icui18n icuuc icudata; do
+              patchelf --add-needed "lib''${icu_lib}.so.$icu_major_v" "$elf"
+            done
+          }
+          patchelf_common() {
+            declare elf="''${1?}"
+
+            chmod +x "$elf"
+            patchelf_add_icu_as_needed "$elf"
+            patchelf --add-needed "libssl.so" "$elf"
+            patchelf --add-needed "libz.so.1" "$elf"
+            patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+              --set-rpath "${lib.makeLibraryPath [stdenv.cc.cc openssl zlib icu.out]}:\$ORIGIN" \
+              "$elf"
+          }
+
+          sed -i -E -e 's/(e.extensionPath,"cache")/require("os").homedir(),".cache","Microsoft", "csdevkit","cache"/g' "$PWD/dist/extension.js"
+          sed -i -E -e 's/o\.chmod/console.log/g' "$PWD/dist/extension.js"
+
+          declare platform
+          case "${stdenv.system}" in
+            "x86_64-linux") platform="linux-x64" ;;
+            "aarch64-linux") platform="linux-arm64" ;;
+            "x86_64-darwin") platform="darwin-x64" ;;
+            "aarch64-darwin") platform="darwin-arm64" ;;
+            *) echo "Unsupported platform: ${stdenv.system}"; exit 1 ;;
+          esac
+
+          declare new_rpath="${lib.makeLibraryPath [stdenv.cc.cc openssl zlib icu.out]}:\$ORIGIN"
+
+          declare base_path="./components/vs-green-server/platforms/$platform/node_modules"
+
+          $patchCommand $add_rpath_command "$new_rpath" "$base_path/@microsoft/visualstudio-server.$platform/Microsoft.VisualStudio.Code.Server"
+          $patchCommand $add_rpath_command "$new_rpath" "$base_path/@microsoft/servicehub-controller-net60.$platform/Microsoft.ServiceHub.Controller"
+          $patchCommand $add_rpath_command "$new_rpath" "$base_path/@microsoft/visualstudio-code-servicehost.$platform/Microsoft.VisualStudio.Code.ServiceHost"
+          $patchCommand $add_rpath_command "$new_rpath" "$base_path/@microsoft/visualstudio-reliability-monitor.$platform/Microsoft.VisualStudio.Reliability.Monitor"
+        '';
+      }))
+      # Cannot activate the 'C# Dev Kit' extension because it depends on the '.NET Install Tool' extension from 'Microsoft', which is not installed. Would you like to install the extension and reload the window?
+      (ms-dotnettools.vscode-dotnet-runtime.overrideAttrs (_: {
+        sourceRoot = "extension";
+        postPatch = ''
+          chmod +x "$PWD/dist/install scripts/dotnet-install.sh"
+        '';
+      }))
+      (ms-dotnettools.vscodeintellicode-csharp.overrideAttrs (_: {sourceRoot = "extension";}))
 
       ## Java
       redhat.java # Java language support
