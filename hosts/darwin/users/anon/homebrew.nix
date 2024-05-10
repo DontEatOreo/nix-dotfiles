569732981dc3faa5044405c2a583384bd89af2ed
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-05-10 03:39:48 +0300

    feat(homebrew): replace rider-eap with rider
    The Rider Early Acess Program has ended, and you cannot use Rider EAP
    onnce the program has ended.

--- hosts/darwin/users/anon/homebrew.nix
+++ hosts/darwin/users/anon/homebrew.nix
@@ -10,7 +10,7 @@
       cleanup = "uninstall";
     };
     taps = [
-      "dahlia/jetbrains-eap" # https://github.com/dahlia/homebrew-jetbrains-eap
+      # "dahlia/jetbrains-eap" # https://github.com/dahlia/homebrew-jetbrains-eap
       "homebrew/cask-fonts"
     ];
     brews = [
@@ -41,7 +41,7 @@
       "thunderbird" # Email client
 
       # Dev
-      "rider-eap"
+      "rider"
       "warp" # Modern Terminal
       "postman" # Create HTTP requests
       "font-monaspace" # Github Fonts
