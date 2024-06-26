67dc1d72e2acaf3081d45f824346605e85e65533
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-06-26 23:17:18 +0300

    chore(hm): fix nesting

--- home-manager/programs/programs.nix
+++ home-manager/programs/programs.nix
@@ -1,31 +1,33 @@
 # Any program.something that isn't too long goes here!
 _: {
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    nix-direnv.enable = true;
-  };
-  programs.git = {
-    enable = true;
-    ignores = [
-      ".DS_Store"
-      "Thumbs.db"
-      "*.DS_Store"
-      "*.swp"
-    ];
-    userName = "DontEatOreo";
-    userEmail = "57304299+DontEatOreo@users.noreply.github.com";
-    signing = {
-      signByDefault = true;
-      key = "0DB5361BEEE530AB";
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
     };
-  };
-  programs.gh.enable = true;
-  progrmas.gitui.enable = true;
-  programs.thefuck = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
+    git = {
+      enable = true;
+      ignores = [
+        ".DS_Store"
+        "Thumbs.db"
+        "*.DS_Store"
+        "*.swp"
+      ];
+      userName = "DontEatOreo";
+      userEmail = "57304299+DontEatOreo@users.noreply.github.com";
+      signing = {
+        signByDefault = true;
+        key = "0DB5361BEEE530AB";
+      };
+    };
+    gh.enable = true;
+    gitui.enable = true;
+    thefuck = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
   };
 }
