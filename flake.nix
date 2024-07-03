69927c9dc3e4e3a8870268b43ea404a40cf5073a
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-07-03 14:57:11 +0300

    feat(flake.nix): add cachix as substituter

--- flake.nix
+++ flake.nix
@@ -29,6 +29,13 @@
     xremap-flake.inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs =
     {
       self,
