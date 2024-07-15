7eac08e4c2433a772fee7453aed003c3f2e42524
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-07-15 15:23:30 +0300

    shared/yt-dlp-script: format with shfmt

--- shared/yt-dlp-script.sh
+++ shared/yt-dlp-script.sh
@@ -15,77 +15,104 @@ final_args="--ignore-config"
 
 # Format to arguments mapping
 declare -A FORMAT_ARGS=(
-  ["m4a"]="${AUDIO_ARGS[*]} --audio-format m4a --embed-metadata"
-  ["mp3"]="${AUDIO_ARGS[*]} --audio-format mp3 --embed-metadata"
-  ["mp4"]="${VIDEO_ARGS[*]} --embed-metadata"
-  ["m4a-cut"]="${AUDIO_ARGS[*]} --audio-format m4a --download-sections"
-  ["mp3-cut"]="${AUDIO_ARGS[*]} --audio-format mp3 --download-sections"
-  ["mp4-cut"]="${VIDEO_ARGS[*]} --download-sections"
+	["m4a"]="${AUDIO_ARGS[*]} --audio-format m4a --embed-metadata"
+	["mp3"]="${AUDIO_ARGS[*]} --audio-format mp3 --embed-metadata"
+	["mp4"]="${VIDEO_ARGS[*]} --embed-metadata"
+	["m4a-cut"]="${AUDIO_ARGS[*]} --audio-format m4a --download-sections"
+	["mp3-cut"]="${AUDIO_ARGS[*]} --audio-format mp3 --download-sections"
+	["mp4-cut"]="${VIDEO_ARGS[*]} --download-sections"
 )
 
 # Check if required tools are installed
 check_dependencies() {
-  command -v ffmpeg >/dev/null 2>&1 || { echo "Error: ffmpeg is not installed."; exit 1; }
-  command -v yt-dlp >/dev/null 2>&1 || { echo "Error: yt-dlp is not installed."; exit 1; }
-  command -v jq >/dev/null 2>&1 || { echo "Error: jq is not installed."; exit 1; }
-  command -v bc >/dev/null 2>&1 || { echo "Error: bc is not installed."; exit 1; }
+	command -v ffmpeg >/dev/null 2>&1 || {
+		echo "Error: ffmpeg is not installed."
+		exit 1
+	}
+	command -v yt-dlp >/dev/null 2>&1 || {
+		echo "Error: yt-dlp is not installed."
+		exit 1
+	}
+	command -v jq >/dev/null 2>&1 || {
+		echo "Error: jq is not installed."
+		exit 1
+	}
+	command -v bc >/dev/null 2>&1 || {
+		echo "Error: bc is not installed."
+		exit 1
+	}
 }
 
 # Parse arguments
 parse_arguments() {
-  local argument="$1"
-  local urlRegex="^(http|https)://"
-  local timeRangeRegex="^([0-9]+(\.[0-9]+)?)-([0-9]+(\.[0-9]+)?)$"
+	local argument="$1"
+	local urlRegex="^(http|https)://"
+	local timeRangeRegex="^([0-9]+(\.[0-9]+)?)-([0-9]+(\.[0-9]+)?)$"
 
-  if [[ $argument =~ $urlRegex ]]; then
-    url="$argument"
-  elif [[ $argument =~ $timeRangeRegex ]]; then
-    timeRange="${argument}"
-  elif [[ $argument == *"-cut"* ]]; then
-    cutOption=true
-    format="${argument}"
-  else
-    if [[ -n "${FORMAT_ARGS[$argument]}" ]]; then
-      format="$argument"
-    else
-      final_args+=" $argument"
-    fi
-  fi
+	if [[ $argument =~ $urlRegex ]]; then
+		url="$argument"
+	elif [[ $argument =~ $timeRangeRegex ]]; then
+		timeRange="${argument}"
+	elif [[ $argument == *"-cut"* ]]; then
+		cutOption=true
+		format="${argument}"
+	else
+		if [[ -n "${FORMAT_ARGS[$argument]}" ]]; then
+			format="$argument"
+		else
+			final_args+=" $argument"
+		fi
+	fi
 }
 
 # Validate required arguments
 check_arguments() {
-  [[ -z "$url" ]] && { echo "Error: Missing URL"; exit 1; }
+	[[ -z "$url" ]] && {
+		echo "Error: Missing URL"
+		exit 1
+	}
 
-  if [[ "$cutOption" == true ]]; then
-    [[ -z "$timeRange" ]] && { echo "Error: Missing time range for -cut option"; exit 1; }
+	if [[ "$cutOption" == true ]]; then
+		[[ -z "$timeRange" ]] && {
+			echo "Error: Missing time range for -cut option"
+			exit 1
+		}
 
-    local startTime endTime
-    startTime=$(echo "$timeRange" | cut -d'-' -f1)
-    endTime=$(echo "$timeRange" | cut -d'-' -f2)
+		local startTime endTime
+		startTime=$(echo "$timeRange" | cut -d'-' -f1)
+		endTime=$(echo "$timeRange" | cut -d'-' -f2)
 
-    (( $(echo "$startTime > $endTime" | bc -l) )) && { echo "Error: Start time is greater than end time in time range"; exit 1; }
-    (( $(echo "$startTime < 0" | bc -l) )) && { echo "Error: Start time cannot be negative"; exit 1; }
+		(($(echo "$startTime > $endTime" | bc -l))) && {
+			echo "Error: Start time is greater than end time in time range"
+			exit 1
+		}
+		(($(echo "$startTime < 0" | bc -l))) && {
+			echo "Error: Start time cannot be negative"
+			exit 1
+		}
 
-    # Check if the end time is valid
-    duration=$(yt-dlp "$url" -j | jq '.duration')
-    (( $(echo "$endTime > $duration" | bc -l) )) && { echo "Error: End time exceeds video duration"; exit 1; }
-  fi
+		# Check if the end time is valid
+		duration=$(yt-dlp "$url" -j | jq '.duration')
+		(($(echo "$endTime > $duration" | bc -l))) && {
+			echo "Error: End time exceeds video duration"
+			exit 1
+		}
+	fi
 }
 
 # Execute yt-dlp with the constructed argument set
 execute_yt_dlp() {
-  local formatArgs
-  IFS=' ' read -r -a formatArgs <<< "${FORMAT_ARGS[$format]}"
-  [[ "$cutOption" == true ]] && formatArgs+=("*${timeRange}" "--force-keyframes-at-cuts")
-  IFS=' ' read -r -a final_args_array <<< "$final_args"
+	local formatArgs
+	IFS=' ' read -r -a formatArgs <<<"${FORMAT_ARGS[$format]}"
+	[[ "$cutOption" == true ]] && formatArgs+=("*${timeRange}" "--force-keyframes-at-cuts")
+	IFS=' ' read -r -a final_args_array <<<"$final_args"
 
-  yt-dlp "$url" "${formatArgs[@]}" "${COMMON_ARGS[@]}" "${OUTPUT_ARGS[@]}" "${final_args_array[@]}"
+	yt-dlp "$url" "${formatArgs[@]}" "${COMMON_ARGS[@]}" "${OUTPUT_ARGS[@]}" "${final_args_array[@]}"
 }
 
 # Main
 for arg in "$@"; do
-  parse_arguments "$arg"
+	parse_arguments "$arg"
 done
 
 check_dependencies
