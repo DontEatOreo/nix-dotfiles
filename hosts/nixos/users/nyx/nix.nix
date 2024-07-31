5844fac82b67492f99245acad9b64cf8e5d6febc
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-07-31 16:16:45 +0300

    nixos/nix: opinionated settings

--- hosts/nixos/users/nyx/nix.nix
+++ hosts/nixos/users/nyx/nix.nix
@@ -1,18 +1,19 @@
-{ inputs, lib, ... }:
+{
+  inputs,
+  lib,
+  config,
+  ...
+}:
 {
   nix = {
-    # Add each flake input as a registery, to make nix3 commands consistent with flake.nix
-    registry = (lib.mapAttrs (_: flake: { inherit flake; })) (
-      (lib.filterAttrs (_: lib.isType "flake")) inputs
-    );
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = [ "/etc/nix/path" ];
     settings = {
       # Enable flakes and new 'nix3' command
       experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+
       auto-optimise-store = true;
       trusted-substituters = [
         "https://devenv.cachix.org"
@@ -25,5 +26,17 @@
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       ];
     };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) (
+      # Flake Inputs
+      lib.filterAttrs (_: lib.isType "flake") inputs
+    );
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") (
+      # Flake Inputs
+      lib.filterAttrs (_: lib.isType "flake") inputs
+    );
   };
 }
