beaf2ee97078176a524b9733d1ebf39ede490608
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-08-17 19:30:36 +0300

    hosts/nixos/hardware: add `hardware.{enable{,32bit},extraPacakges{,32}}`

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -1,20 +1,34 @@
-{ config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 {
   hardware = {
     bluetooth = {
       enable = true;
       powerOnBoot = true;
     };
-    graphics.enable = true;
+    graphics = {
+      enable = true;
+      # For Wine32 mostly
+      enable32Bit = true;
+      extraPackages = lib.attrValues {
+        inherit (pkgs)
+          mesa
+          nvidia-vaapi-driver
+          # For Encoding/Decoding Videos
+          nv-codec-headers-12
+          vulkan-loader
+          ;
+      };
+      extraPackages32 = lib.attrValues { inherit (pkgs.driversi686Linux) mesa; };
+    };
     nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.production;
-
       # Modesetting is required.
       modesetting.enable = true;
-
-      # Enable the Nvidia settings menu,
-      # accessible via `nvidia-settings`.
-      nvidiaSettings = true;
     };
   };
 }
