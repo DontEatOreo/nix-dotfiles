8bbfbaff53f3eb310f0cb7e0627c42b31e2b5adb
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-09-19 15:57:48 +0300

    hosts/darwin/{configuration,default}: `configuration.nix` -> `default.nix`
    - Since `configuration.nix` is so small it can just live in the
      `default.nix`

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -10,15 +10,34 @@ in
       inherit inputs system username;
     };
     modules = [
-      ./configuration.nix
-      ../../modules/common
       inputs.home-manager.darwinModules.home-manager
       inputs.lix-module.nixosModules.default
+
+      ./fonts.nix
       ./home.nix
+      ./homebrew.nix
+      ./system.nix
+      ./zshrc.nix
+
+      ../../shared/cli.nix
+      ../../shared/dev.nix
+      ../../shared/gnuimp.nix
+      ../../shared/tui.nix
+      ../../shared/programs.nix
       {
+        services.nix-daemon.enable = true;
+
         users.users.${username} = {
           name = username;
           home = "/Users/${username}";
+        };
+      }
+
+      ../../modules/common
+      {
+        shared = {
+          nix.enable = true;
+          nixpkgs.enable = true;
         };
       }
     ];
