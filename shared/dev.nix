9ad5f8a42348cff4d5500142eb014e72625368f7
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-09-23 22:50:58 +0300

    shared/dev: remove `direnv` pkg
    Since `programs.direnv` already installs, it it's pointless to have it
    separately

--- shared/dev.nix
+++ shared/dev.nix
@@ -5,7 +5,6 @@
       inherit (pkgs)
         github-copilot-cli # GitHub Copilot CLI
         jq
-        direnv
         devenv # Manage development environment on Nix
         nixfmt-rfc-style # Upcomming new nix format, RFC 0160
         shellcheck # Warning hints for shell scripts
