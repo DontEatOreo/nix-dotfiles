82e0cb774c0e24ef38b6520aba26a216d087e0fa
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-09-23 23:01:17 +0300

    {hosts/nixos/configuration,shared/latex}: drop `latex`
    I mostly write LaTeX on Overleaf, so it's kind of pointless to have it
    locally, especially since I haven't used it in so long

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -19,7 +19,6 @@
     ../../shared/cli.nix
     ../../shared/dev.nix
     ../../shared/gnuimp.nix
-    #../../shared/latex.nix # Disabled by default due to file size
     ../../shared/tui.nix
     ../../shared/programs.nix
   ];
--- shared/latex.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-{
-  environment = {
-    systemPackages = builtins.attrValues {
-      text-live-full = pkgs.texlive.combined.scheme-full;
-      inherit (pkgs) pandoc;
-    };
-  };
-}
