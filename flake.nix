fb01165fc07b6611e112b4abeb387a5da8fab8a2
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-09-25 14:42:01 +0300

    flake{lock,-}: bump `nix-vscode-extensions`

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     nixvim.url = "github:elythh/nixvim";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/03d3171c94c36f43c10c46df6fbab127af314da6";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/fb86a415579cd38eb7b47c3ada597841b97e2ea9";
     nix-vscode-extensions.inputs.nixpkgs.follows = "nixpkgs";
 
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
