939ff0f0cc95283ba79151ca302d04eb49170c4d
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-10-03 15:04:20 +0300

    modules/hm/browsers/firefox: add search engines

--- modules/home-manager/browsers/firefox.nix
+++ modules/home-manager/browsers/firefox.nix
@@ -32,6 +32,110 @@ in
             violentmonkey # Browser Scripts
             ;
         };
+        search = {
+          force = true;
+          order = [
+            "GitHub"
+            "Google"
+            "Home Manager"
+            "Kagi"
+            "Nix Packages"
+            "Nix"
+            "NixOS Wiki"
+            "YouTube"
+          ];
+          privateDefault = "Kagi";
+          engines = {
+            "GitHub" = {
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://github.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@gh" ];
+            };
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+            "Home Manager" = {
+              url = [
+                {
+                  template = "https://mipmip.github.io/home-manager-option-search/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@hm" ];
+            };
+            "Kagi " = {
+              urls = [
+                {
+                  template = "https://kagi.com/";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://kagi.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@k" ];
+            };
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "NixOS Wiki" = {
+              urls = [ { template = "https://wiki.nixos.org/index.php?search={searchTerms}"; } ];
+              icon = "https://wiki.nixos.org/favicon.png";
+              definedAliases = [ "@nw" ];
+            };
+            "YouTube" = {
+              urls = [
+                {
+                  template = "https://www.youtube.com/results";
+                  params = [
+                    {
+                      name = "search_query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://youtube.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@yt" ];
+            };
+          };
+        };
       };
       policies = {
         DisableTelemetry = true;
