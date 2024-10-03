33ce476d1a7184467cf90f36d8c35a368f19ca08
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-10-03 15:04:20 +0300

    modules/hm/browsers/firefox: add search engines

--- modules/home-manager/browsers/firefox.nix
+++ modules/home-manager/browsers/firefox.nix
@@ -32,6 +32,88 @@ in
             violentmonkey # Browser Scripts
             ;
         };
+        search = {
+          force = true;
+          default = "Kagi";
+          privateDefault = "Kagi";
+          order = [
+            "GitHub"
+            "Google"
+            "Kagi"
+            "Nix Packages"
+            "YouTube"
+          ];
+          engines = {
+            "GitHub" = {
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://github.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@gh" ];
+            };
+            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+            "Kagi " = {
+              urls = [
+                {
+                  template = "https://kagi.com/search";
+                  params = [
+                    {
+                      name = "q";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://assets.kagi.com/v2/apple-touch-icon.png";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@k" ];
+            };
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = [ "@np" ];
+            };
+            "YouTube" = {
+              urls = [
+                {
+                  template = "https://www.youtube.com/results";
+                  params = [
+                    {
+                      name = "search_query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              iconUpdateURL = "https://youtube.com/favicon.ico";
+              updateInterval = 24 * 60 * 60 * 1000;
+              definedAliases = [ "@yt" ];
+            };
+          };
+        };
       };
       policies = {
         DisableTelemetry = true;
