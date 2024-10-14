d072a6633e2f7d2fa2aabe078543823457ba42b7
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-10-14 16:15:38 +0300

    nixcord: replace deprecated `searchReply` with `fullSearchContext`

--- modules/home-manager/guis/nixcord.nix
+++ modules/home-manager/guis/nixcord.nix
@@ -96,7 +96,7 @@
           revealAllSpoilers.enable = true;
           reverseImageSearch.enable = true;
           roleColorEverywhere.enable = true;
-          searchReply.enable = true;
+          fullSearchContext.enable = true;
           serverInfo.enable = true;
           serverListIndicators.enable = true;
           shikiCodeblocks.enable = true;
