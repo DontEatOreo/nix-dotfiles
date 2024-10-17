c1dbe8cc1cc906d33ab6142575f2e917214cfcd7
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-10-17 16:13:28 +0300

    shared/dev: format alphabetically

--- shared/dev.nix
+++ shared/dev.nix
@@ -1,19 +1,15 @@
 { pkgs, ... }:
 {
-  environment = {
-    systemPackages = builtins.attrValues {
-      inherit (pkgs)
-        github-copilot-cli # GitHub Copilot CLI
-        jq
-        nixfmt-rfc-style # Upcomming new nix format, RFC 0160
-        nixpkgs-review
-        shellcheck # Warning hints for shell scripts
-
-        # Terminal related packages
-        delta # Better Diff
-        bc # Command line calculator
-        nil # Nix language server
-        ;
-    };
+  environment.systemPackages = builtins.attrValues {
+    inherit (pkgs)
+      bc # Command line calculator
+      delta # Better Diff
+      github-copilot-cli # GitHub Copilot CLI
+      jq
+      nil # Nix language server
+      nixfmt-rfc-style
+      nixpkgs-review
+      shellcheck # Warning hints for shell scripts
+      ;
   };
 }
