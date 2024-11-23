14c0c4e102bf46f9f43135f3823942328388f43c
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-11-23 18:51:02 +0200

    hosts/{darwin,nixos}/home: format

--- hosts/darwin/home.nix
+++ hosts/darwin/home.nix
@@ -11,67 +11,69 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.${username} = { config, ... }: {
-      imports = [
-        ../../modules/home-manager/cli
-        ../../modules/home-manager/guis
-        ../../modules/home-manager/terminals/bash.nix
-        ../../modules/home-manager/tui
-        inputs.sops-nix.homeManagerModules.sops
-        inputs.catppuccin.homeManagerModules.catppuccin
-        {
-          catppuccin = {
-            enable = true;
-            flavor = "frappe";
-            accent = "blue";
-          };
-        }
-        {
-          home.file.".warp/themes".source =
-            (pkgs.callPackage ../../modules/home-manager/terminals/warp-terminal-catppuccin.nix {
-              inherit (config.catppuccin) accent;
-            }).outPath
-            + "/share/warp/themes";
-        }
-
-        {
-          hm = {
-            bash.enable = true;
-            direnv.enable = true;
-            fastfetch.enable = true;
-            fzf.enable = true;
-            git.enable = true;
-            nixcord = {
+    users.${username} =
+      { config, ... }:
+      {
+        imports = [
+          ../../modules/home-manager/cli
+          ../../modules/home-manager/guis
+          ../../modules/home-manager/terminals/bash.nix
+          ../../modules/home-manager/tui
+          inputs.sops-nix.homeManagerModules.sops
+          inputs.catppuccin.homeManagerModules.catppuccin
+          {
+            catppuccin = {
               enable = true;
-              theme = {
-                dark = {
-                  flavor = config.catppuccin.flavor;
-                  accent = config.catppuccin.accent;
-                };
-                light = {
-                  flavor = "latte";
-                  accent = config.catppuccin.accent;
+              flavor = "frappe";
+              accent = "blue";
+            };
+          }
+          {
+            home.file.".warp/themes".source =
+              (pkgs.callPackage ../../modules/home-manager/terminals/warp-terminal-catppuccin.nix {
+                inherit (config.catppuccin) accent;
+              }).outPath
+              + "/share/warp/themes";
+          }
+
+          {
+            hm = {
+              bash.enable = true;
+              direnv.enable = true;
+              fastfetch.enable = true;
+              fzf.enable = true;
+              git.enable = true;
+              nixcord = {
+                enable = true;
+                theme = {
+                  dark = {
+                    flavor = config.catppuccin.flavor;
+                    accent = config.catppuccin.accent;
+                  };
+                  light = {
+                    flavor = "latte";
+                    accent = config.catppuccin.accent;
+                  };
                 };
               };
+              ssh.enable = true;
+              vscode.enable = true;
+              yazi.enable = true;
             };
-            ssh.enable = true;
-            vscode.enable = true;
-            yazi.enable = true;
-          };
 
-          sops = {
-            age.keyFile = "/Users/${username}/Library/Application Support/sops/age/keys.txt";
-            defaultSopsFile = ../../secrets/secrets.yaml;
-            secrets.github_ssh = { };
-          };
+            sops = {
+              age.keyFile = "/Users/${username}/Library/Application Support/sops/age/keys.txt";
+              defaultSopsFile = ../../secrets/secrets.yaml;
+              secrets.github_ssh = { };
+            };
 
-          home = {
-            stateVersion = "24.05";
-            packages = [ inputs.nixvim.packages.${system}.default ];
-          };
-        }
-      ];
-    };
+            home = {
+              stateVersion = "24.05";
+              packages = [ inputs.nixvim.packages.${system}.default ];
+            };
+          }
+        ];
+      };
     extraSpecialArgs = {
       inherit inputs system;
     };
--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -9,65 +9,67 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.${username} = { config, ...}: {
-      imports = [
-        ../../modules/home-manager/browsers
-        ../../modules/home-manager/cli
-        ../../modules/home-manager/config/dconf.nix
-        ../../modules/home-manager/guis
-        ../../modules/home-manager/terminals
-        ../../modules/home-manager/tui
-        inputs.sops-nix.homeManagerModules.sops
-        inputs.catppuccin.homeManagerModules.catppuccin
-        {
-          catppuccin = {
-            enable = true;
-            flavor = "frappe";
-            accent = "blue";
-          };
-        }
-
-        {
-          hm = {
-            bash.enable = true;
-            chromium.enable = true;
-            dconf.enable = true;
-            direnv.enable = true;
-            firefox.enable = true;
-            fzf.enable = true;
-            git.enable = true;
-            mpv.enable = true;
-            nixcord = {
+    users.${username} =
+      { config, ... }:
+      {
+        imports = [
+          ../../modules/home-manager/browsers
+          ../../modules/home-manager/cli
+          ../../modules/home-manager/config/dconf.nix
+          ../../modules/home-manager/guis
+          ../../modules/home-manager/terminals
+          ../../modules/home-manager/tui
+          inputs.sops-nix.homeManagerModules.sops
+          inputs.catppuccin.homeManagerModules.catppuccin
+          {
+            catppuccin = {
               enable = true;
-              theme = {
-                dark = {
-                  inherit (config.catppuccin) flavor accent;
-                };
-                light = {
-                  flavor = "latte";
-                  inherit (config.catppuccin) accent;
+              flavor = "frappe";
+              accent = "blue";
+            };
+          }
+
+          {
+            hm = {
+              bash.enable = true;
+              chromium.enable = true;
+              dconf.enable = true;
+              direnv.enable = true;
+              firefox.enable = true;
+              fzf.enable = true;
+              git.enable = true;
+              mpv.enable = true;
+              nixcord = {
+                enable = true;
+                theme = {
+                  dark = {
+                    inherit (config.catppuccin) flavor accent;
+                  };
+                  light = {
+                    flavor = "latte";
+                    inherit (config.catppuccin) accent;
+                  };
                 };
               };
+              ssh.enable = true;
+              vscode.enable = true;
+              yazi.enable = true;
+              zsh.enable = true;
             };
-            ssh.enable = true;
-            vscode.enable = true;
-            yazi.enable = true;
-            zsh.enable = true;
-          };
 
-          sops = {
-            age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
-            defaultSopsFile = ../../secrets/secrets.yaml;
-            secrets.github_ssh = { };
-          };
+            sops = {
+              age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+              defaultSopsFile = ../../secrets/secrets.yaml;
+              secrets.github_ssh = { };
+            };
 
-          home = {
-            stateVersion = "24.05";
-            packages = [ inputs.nixvim.packages.${system}.default ];
-          };
-        }
-      ];
-    };
+            home = {
+              stateVersion = "24.05";
+              packages = [ inputs.nixvim.packages.${system}.default ];
+            };
+          }
+        ];
+      };
     extraSpecialArgs = {
       inherit inputs system;
     };
