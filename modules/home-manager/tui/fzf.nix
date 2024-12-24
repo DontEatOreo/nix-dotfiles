2a590101e9114c6c623ae98a101ac48121696206
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-12-24 21:19:50 +0200

    modules/hm/tui/fzf: remove explicit `enable{Bash,Zsh}Integration`
    They're enabled by default

--- modules/home-manager/tui/fzf.nix
+++ modules/home-manager/tui/fzf.nix
@@ -2,11 +2,5 @@
 {
   options.hm.fzf.enable = lib.mkEnableOption "FZF";
 
-  config = lib.mkIf config.hm.fzf.enable {
-    programs.fzf = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-    };
-  };
+  config = lib.mkIf config.hm.fzf.enable { programs.fzf.enable = true; };
 }
