7c6bbe9a1c1acba0d463d0a25977f5895e08726d
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-12-29 15:45:48 +0200

    hosts/darwin/zsh: reorganize shell initialization
    The shell initialization order matters - prompt setup should happen
    before other interactive features load
    
    - Move p10k and theme to `promptInit`
    - Move oh-my-zsh and autosuggestions to `interactiveShellInit`

--- hosts/darwin/zsh.nix
+++ hosts/darwin/zsh.nix
@@ -14,11 +14,12 @@ in
 {
   programs.zsh = {
     enableSyntaxHighlighting = true;
-    loginShellInit = ''
-      source ${pkgs.oh-my-zsh}/share/oh-my-zsh/oh-my-zsh.sh
-
+    promptInit = ''
       source ${../../modules/home-manager/config/p10k.zsh}
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+    '';
+    interactiveShellInit = ''
+      source ${pkgs.oh-my-zsh}/share/oh-my-zsh/oh-my-zsh.sh
       source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
       eval "$(github-copilot-cli alias -- "$0")"
