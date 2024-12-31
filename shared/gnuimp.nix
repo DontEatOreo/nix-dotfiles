d5bee82c97f3b396bd5ecc9965a69f92007a7173
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-12-31 04:19:28 +0200

    shared/gnuimp: update comment format

--- shared/gnuimp.nix
+++ shared/gnuimp.nix
@@ -1,9 +1,9 @@
 /*
-  GNU Implementations of certain programs
-  The rationale behind using a "GNU" implementation instead of the
-  system one is that, for example, on macOS, awk and grep
-  BSD implementations miss certain options, making it more cumbersome
-  to use them compared to their GNU counterpart
+  GNU Implementations of certain programs The rationale behind using a
+  "GNU" implementation instead of the system one is that, for example,
+  on macOS, awk and grep BSD implementations miss certain options,
+  making it more cumbersome to use them compared to their GNU
+  counterpart
 */
 { pkgs, ... }:
 {
