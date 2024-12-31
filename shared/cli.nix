e8958baccd2529ffdad3961f41c598bc2b6d566f
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2024-12-31 04:36:18 +0200

    shared/cli: add rust implementation of common cli tools

--- shared/cli.nix
+++ shared/cli.nix
@@ -1,26 +1,37 @@
 { pkgs, inputs, ... }:
 {
   environment = {
-    systemPackages = builtins.attrValues {
-      inherit (pkgs)
-        # Video Related
-        ffmpeg_7-full
-        yt-dlp # Video Downloader
+    systemPackages =
+      builtins.attrValues {
+        inherit (pkgs)
+          # Video Related
+          ffmpeg_7-full
+          yt-dlp # Video Downloader
 
-        # Images Related
-        gallery-dl # Image Downloader
-        ghostscript # Interpreter for PostScript and PDF (Convert PDF to Image)
-        imagemagick # Image Manipulation
+          # Images Related
+          gallery-dl # Image Downloader
+          ghostscript # Interpreter for PostScript and PDF (Convert PDF to Image)
+          imagemagick # Image Manipulation
 
-        # Other
-        eza # Bettter LS
-        neofetch # Show System Info
-        tldr # Most Common used commands for CLI tool
-        tree
-        wget
-        ;
-      dis = inputs.dis.packages.${pkgs.system}.default;
-    };
+          # Other
+          tldr # Most Common used commands for CLI tool
+          tree
+          wget
+          ;
+        dis = inputs.dis.packages.${pkgs.system}.default;
+      }
+      # Rust implementation of GNU tools
+      ++ builtins.attrValues {
+        inherit (pkgs)
+          bat # cat
+          dust # du
+          eza # ls
+          fd # find
+          ripgrep-all # grep
+          sd # sed
+          zoxide # cd
+          ;
+      };
     variables = {
       EDITOR = "nvim";
     };
