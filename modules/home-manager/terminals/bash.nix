0cc28f06393bbd7457f25a954937e45a923bd5e0
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-02 16:08:56 +0200

    modules/hm/terminals/bash: drop `enableCompletion`, drop misc init

--- modules/home-manager/terminals/bash.nix
+++ modules/home-manager/terminals/bash.nix
@@ -5,12 +5,9 @@
   config = lib.mkIf config.hm.bash.enable {
     programs.bash = {
       enable = true;
-      enableCompletion = true;
+      enableVteIntegration = true;
       initExtra = ''
-        shopt -s autocd
-        set -o vi
         eval "$(github-copilot-cli alias -- "$0")"
-        export PS1="\[$(tput bold)\]\[$(tput setaf 1)\][\[$(tput setaf 3)\]\u\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 5)\]\W\[$(tput setaf 1)\]]\[$(tput setaf 7)\]\\$ \[$(tput sgr0)\]"
       '';
     };
   };
