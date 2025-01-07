5804a1f0a91451eef1d6959c43768389efcac29d
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-07 18:52:07 +0200

    hosts/nixos/sound: drop `10-disable-camera` patch
    It already got fixed on `2024-10-03T21:48:00+00:00` or `Thu, 3 Oct 2024
    21:48:00 +0000 (3 months ago)`

--- hosts/nixos/sound.nix
+++ hosts/nixos/sound.nix
@@ -10,16 +10,6 @@ _: {
     };
     pulse.enable = true;
     wireplumber.extraConfig = {
-      # Patch to fix usb-camera bug
-      # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/2669
-      # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/4115
-      "10-disable-camera" = {
-        "wireplumber.profiles" = {
-          main = {
-            "monitor.libcamera" = "disabled";
-          };
-        };
-      };
       # Fixes the "Corsair HS80 Wireless" Volume desync between Headset & System
       "volume-sync" = {
         "bluez5.enable-absolute-volume" = true;
