5c4f1e5b1d19e1cbc46aef2a6b851b3684639e18
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-11 17:01:22 +0200

    modules/common/nix: drop deprecated substituter`cuda-maintainers`

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -25,12 +25,10 @@ in
         substituters = [
           "https://devenv.cachix.org"
           "https://nix-community.cachix.org"
-          "https://cuda-maintainers.cachix.org"
         ];
         trusted-public-keys = [
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
       # Opinionated: disable channels
