308652832cfdca463f1b7e967c8e392b896ecf63
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-11 17:11:17 +0200

    modules/common/nix: `enableOnSystem` -> `lib.optionalAttrs`

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -6,50 +6,53 @@
   ...
 }:
 let
-  enableOnSystem = x: y: (lib.mkIf (system == x) y);
   isLinux = builtins.match ".*linux.*" system != null;
 in
 {
   options.shared.nix.enable = lib.mkEnableOption "Nix";
 
   config = lib.mkIf config.shared.nix.enable {
-    nix = {
-      settings = {
-        # Enable flakes and new 'nix3' command
-        experimental-features = "nix-command flakes";
-        # Opinionated: disable global registry
-        flake-registry = enableOnSystem isLinux "";
-        # Workaround for https://github.com/NixOS/nix/issues/9574
-        nix-path = enableOnSystem isLinux config.nix.nixPath;
+    nix =
+      {
+        settings =
+          {
+            # Enable flakes and new 'nix3' command
+            experimental-features = "nix-command flakes";
 
-        substituters = [
-          "https://devenv.cachix.org"
-          "https://nix-community.cachix.org"
-        ];
-        trusted-public-keys = [
-          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
+            substituters = [
+              "https://devenv.cachix.org"
+              "https://nix-community.cachix.org"
+            ];
+            trusted-public-keys = [
+              "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+              "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
+          }
+          // lib.optionalAttrs isLinux {
+            # Opinionated: disable global registry
+            flake-registry = "";
+            # Workaround for https://github.com/NixOS/nix/issues/9574
+            nix-path = config.nix.nixPath;
+          };
+        # Opinionated: disable channels
+        channel.enable = false;
+
+        # Opinionated: make flake registry and nix path match flake inputs
+        registry = lib.mapAttrs (_: flake: { inherit flake; }) (
+          # Flake Inputs
+          lib.filterAttrs (_: lib.isType "flake") inputs
+        );
+        nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") (
+          # Flake Inputs
+          lib.filterAttrs (_: lib.isType "flake") inputs
+        );
+      }
+      // lib.optionalAttrs (!isLinux) {
+        extraOptions = ''
+          experimental-features = nix-command flakes
+          builders-use-substitutes = true
+          build-use-substitutes = true
+        '';
       };
-      # Opinionated: disable channels
-      channel.enable = false;
-
-      extraOptions = enableOnSystem "aarch64-darwin" ''
-        experimental-features = nix-command flakes
-        builders-use-substitutes = true
-        build-use-substitutes = true
-      '';
-
-      # Opinionated: make flake registry and nix path match flake inputs
-      registry = lib.mapAttrs (_: flake: { inherit flake; }) (
-        # Flake Inputs
-        lib.filterAttrs (_: lib.isType "flake") inputs
-      );
-      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") (
-        # Flake Inputs
-        lib.filterAttrs (_: lib.isType "flake") inputs
-      );
-    };
   };
-
 }
