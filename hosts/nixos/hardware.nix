88d39e44c92dd7f8a2b50875899951370631f59f
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-19 16:02:24 +0200

    hosts/nixos/hardware: flatten attrs

--- hosts/nixos/hardware.nix
+++ hosts/nixos/hardware.nix
@@ -1,19 +1,14 @@
 { config, ... }:
 {
   hardware = {
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-    };
-    graphics = {
-      enable = true;
-      # For Wine32 mostly
-      enable32Bit = true;
-    };
-    nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-      # Modesetting is required.
-      modesetting.enable = true;
-    };
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+
+    graphics.enable = true;
+    graphics.enable32Bit = true;
+
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
+    # Modesetting is required.
+    nvidia.modesetting.enable = true;
   };
 }
