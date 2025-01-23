e6506cc4d50f9253f1b75645edf6946ba925a2b6
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-01-23 23:37:50 +0200

    modules/hm/cli/git: switch to using ssh signing for commits
    Also add a `mkOutOfStoreSymlink` because macOS loves to not honor
    XDG_CONFIG...

--- modules/home-manager/cli/git.nix
+++ modules/home-manager/cli/git.nix
@@ -1,8 +1,24 @@
-{ lib, config, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  userName = "DontEatOreo";
+  userEmail = "57304299+${userName}@users.noreply.github.com";
+  key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPsZFHUhLSPiz0EF1Q59jzu7IS7qdn3MSEImztN4KgmN";
+in
 {
   options.hm.git.enable = lib.mkEnableOption "Git";
 
   config = lib.mkIf config.hm.git.enable {
+    home.file.".gitconfig".source =
+      config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/git/config";
+    home.file.".ssh/allowed_signers".text = ''
+      ${userEmail} ${key}
+    '';
+
     programs = {
       gitui.enable = true;
       gh.enable = true;
@@ -15,11 +31,19 @@
           "*.DS_Store"
           "*.swp"
         ];
-        userName = "DontEatOreo";
-        userEmail = "57304299+DontEatOreo@users.noreply.github.com";
-        signing = {
-          signByDefault = true;
-          key = "0DB5361BEEE530AB";
+        inherit userName userEmail;
+        signing.signByDefault = true;
+        signing.key = key;
+        extraConfig = {
+          gpg.format = "ssh";
+          "gpg.ssh" = {
+            program =
+              if pkgs.stdenvNoCC.hostPlatform.isLinux then
+                (lib.getExe' pkgs._1password-gui "op-ssh-sign")
+              else
+                "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+            allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
+          };
         };
       };
       vscode.userSettings."git.enableCommitSigning" =
