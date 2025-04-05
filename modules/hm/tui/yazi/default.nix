98ddcdffd20b914309e45ccc8a3d768f85ff43d9
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-04-06 00:57:43 +0300

    modules/hm/tui/yazi/default: bump `system-clipboard`

--- modules/hm/tui/yazi/default.nix
+++ modules/hm/tui/yazi/default.nix
@@ -43,8 +43,8 @@
           system-clipboard = pkgs.fetchFromGitHub {
             owner = "orhnk";
             repo = "system-clipboard.yazi";
-            rev = "7775a80e8d3391e0b3da19ba143196960a4efc48";
-            hash = "sha256-tfR9XHvRqm7yPbTu/joBDpu908oceaUoBiIImehMobk=";
+            rev = "efb8f03e632adcdc6677fd5f471c74f4c71fdf9a";
+            hash = "sha256-zOQQvbkXq71t2E4x45oM4MzVRlZ4hhe6RkvgcP8tdYE=";
           };
         };
       initLua = ''
