a093f39e06e2cf829da69fd207dc741dcda531fc
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-04-26 19:14:42 +0300

    modules/hm/tui/helix/languages: move `languages` to `let ... in`

--- modules/hm/tui/helix/languages.nix
+++ modules/hm/tui/helix/languages.nix
@@ -1,5 +1,102 @@
 { pkgs, lib, ... }:
 let
+
+  language-server = {
+    bash-language-server = {
+      args = [ "start" ];
+      command = lib.getExe pkgs.bash-language-server;
+      config.enable = true;
+    };
+    nil = {
+      command = "nil";
+      config.nil.formatting.command = [ (lib.getExe pkgs.nixfmt-rfc-style) ];
+    };
+    ruff = {
+      command = lib.getExe pkgs.ruff;
+      args = [
+        "server"
+        "--preview"
+      ];
+      config = {
+        lineLength = 100;
+        lint.extendSelect = [ "I" ];
+      };
+    };
+    deno = {
+      command = lib.getExe pkgs.deno;
+      args = [ "lsp" ];
+      config = {
+        enable = true;
+        lint = true;
+        unstable = true;
+        format = {
+          options = {
+            lineWidth = 100;
+            indentWidth = 2;
+          };
+        };
+        javascript = {
+          format = {
+            options = {
+              indentWidth = 4;
+            };
+          };
+        };
+        typescript = {
+          format = {
+            options = {
+              indentWidth = 4;
+            };
+          };
+        };
+        suggest = {
+          imports = {
+            hosts = {
+              "https://deno.land" = true;
+              "https://cdn.nest.land" = true;
+              "https://crux.land" = true;
+            };
+          };
+        };
+        inlayHints = {
+          enumMemberValues.enabled = true;
+          functionLikeReturnTypes.enabled = true;
+          parameterNames.enabled = "all";
+          parameterTypes.enabled = true;
+          propertyDeclarationTypes.enabled = true;
+          variableTypes.enabled = true;
+        };
+      };
+    };
+    yaml-language-server = {
+      command = lib.getExe pkgs.yaml-language-server;
+      args = [ "--stdio" ];
+      config = {
+        yaml = {
+          format = {
+            enable = true;
+          };
+          validation = true;
+          schemas = {
+            https = true;
+          };
+        };
+      };
+    };
+    taplo = {
+      command = lib.getExe pkgs.taplo;
+      args = [
+        "lsp"
+        "stdio"
+      ];
+      config = {
+        formatter = {
+          alignEntries = true;
+          columnWidth = 100;
+        };
+      };
+    };
+  };
   language = [
     {
       name = "python";
@@ -66,104 +163,5 @@ let
   ];
 in
 {
-  programs.helix.languages = {
-    language-server = {
-      bash-language-server = {
-        args = [ "start" ];
-        command = lib.getExe pkgs.bash-language-server;
-        config.enable = true;
-      };
-      nil = {
-        command = "nil";
-        config.nil.formatting.command = [ (lib.getExe pkgs.nixfmt-rfc-style) ];
-      };
-      ruff = {
-        command = lib.getExe pkgs.ruff;
-        args = [
-          "server"
-          "--preview"
-        ];
-        config = {
-          lineLength = 100;
-          lint.extendSelect = [ "I" ];
-        };
-      };
-
-      deno = {
-        command = lib.getExe pkgs.deno;
-        args = [ "lsp" ];
-        config = {
-          enable = true;
-          lint = true;
-          unstable = true;
-          format = {
-            options = {
-              lineWidth = 100;
-              indentWidth = 2;
-            };
-          };
-          javascript = {
-            format = {
-              options = {
-                indentWidth = 4;
-              };
-            };
-          };
-          typescript = {
-            format = {
-              options = {
-                indentWidth = 4;
-              };
-            };
-          };
-          suggest = {
-            imports = {
-              hosts = {
-                "https://deno.land" = true;
-                "https://cdn.nest.land" = true;
-                "https://crux.land" = true;
-              };
-            };
-          };
-          inlayHints = {
-            enumMemberValues.enabled = true;
-            functionLikeReturnTypes.enabled = true;
-            parameterNames.enabled = "all";
-            parameterTypes.enabled = true;
-            propertyDeclarationTypes.enabled = true;
-            variableTypes.enabled = true;
-          };
-        };
-      };
-      yaml-language-server = {
-        command = lib.getExe pkgs.yaml-language-server;
-        args = [ "--stdio" ];
-        config = {
-          yaml = {
-            format = {
-              enable = true;
-            };
-            validation = true;
-            schemas = {
-              https = true;
-            };
-          };
-        };
-      };
-      taplo = {
-        command = lib.getExe pkgs.taplo;
-        args = [
-          "lsp"
-          "stdio"
-        ];
-        config = {
-          formatter = {
-            alignEntries = true;
-            columnWidth = 100;
-          };
-        };
-      };
-    };
-    inherit language;
-  };
+  programs.helix.languages = { inherit language-server language; };
 }
