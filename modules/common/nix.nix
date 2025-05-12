a3df9c64825b66b1ba62f7c8b2c77db80e98a1f6
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-12 16:29:40 +0300

    modules/common/nix: add `helix` cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -22,10 +22,12 @@ in
           substituters = [
             "https://devenv.cachix.org"
             "https://nix-community.cachix.org"
+            "https://helix.cachix.org"
           ];
           trusted-public-keys = [
             "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
           ];
         }
         // lib.optionalAttrs isLinux {
