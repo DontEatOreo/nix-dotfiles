e1ffed047339b9517fa7f88454b849715201f9d6
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-27 18:27:02 +0300

    chore(common/nixpkgs): remove overalys for `qrencode`, `flasgger`

--- modules/common/nixpkgs.nix
+++ modules/common/nixpkgs.nix
@@ -14,31 +14,6 @@ in
     } // lib.optionalAttrs isLinux { nixpkgs.config.cudaSupport = true; };
     overlays = [
       (final: prev: { yt-dlp = final.callPackage ../../shared/yt-dlp.nix { }; })
-      (final: prev: {
-        qrencode = prev.qrencode.overrideAttrs (old: {
-          src = final.fetchFromGitHub {
-            owner = "fukuchi";
-            repo = "libqrencode";
-            rev = "v${old.version}";
-            sha256 = "sha256-nbrmg9SqCqMrLE7WCfNEzMV/eS9UVCKCrjBrGMzAsLk";
-          };
-          nativeBuildInputs = old.nativeBuildInputs ++ [ final.autoreconfHook ];
-          nativeCheckInputs = [ ];
-          doCheck = false;
-        });
-      })
-      # https://github.com/NixOS/nixpkgs/pull/391322
-      (final: prev: {
-        flasgger = prev.flasgger.overrideAttrs (old: {
-          src = final.fetchFromGitHub {
-            owner = "flasgger";
-            repo = "flasgger";
-            rev = "v${old.version}";
-            hash = "sha256-ULEf9DJiz/S2wKlb/vjGto8VCI0QDcm0pkU5rlOwtiE=";
-          };
-          patches = [ ];
-        });
-      })
     ] ++ (lib.optional isLinux inputs.nur.overlays.default);
   };
 }
