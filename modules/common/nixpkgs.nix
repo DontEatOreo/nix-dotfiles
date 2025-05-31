a8a2ff0087e0a73ee6ae7250dab3c1c20c1ca005
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-31 20:02:48 +0300

    chore(flake.*,common/nixpkgs): add lix input, overlay lix

--- modules/common/nixpkgs.nix
+++ modules/common/nixpkgs.nix
@@ -14,6 +14,22 @@ in
     } // lib.optionalAttrs isLinux { nixpkgs.config.cudaSupport = true; };
     overlays = [
       (final: prev: { yt-dlp = final.callPackage ../../pkgs/yt-dlp.nix { }; })
+      (final: prev: {
+        lix =
+          (inputs.lix.packages.${config.nixpkgs.hostPlatform.system}.default.override { aws-sdk-cpp = null; })
+          .overrideAttrs
+            (args: {
+              postPatch =
+                (args.postPatch or "")
+                + ''
+                  substituteInPlace lix/libmain/shared.cc \
+                    --replace-fail "(Lix, like Nix)" "(Lix, like Nix but better)"        
+                '';
+              # Okay, I'm making a huge bet they will never break CI, which
+              # probably hold up
+              doCheck = false;
+            });
+      })
     ] ++ (lib.optional isLinux inputs.nur.overlays.default);
   };
 }
