6007f8db5d32602f14760b40296a097b60cd4b64
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-06-14 12:20:54 +0300

    chore(linux/home): `home-manager.users.*` to use `let ... in ... ++ ...`

--- hosts/linux/home.nix
+++ hosts/linux/home.nix
@@ -1,67 +1,83 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
+
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.nyx =
-      { config, ... }:
-      {
-        imports = [
-          ../../modules/hm/browser
-          ../../modules/hm/cli
-          ../../modules/hm/config/dconf.nix
-          ../../modules/hm/gui
-          ../../modules/hm/terminal
-          ../../modules/hm/tui
-          ../../shared/aliases.nix
-          inputs.catppuccin.homeModules.catppuccin
-          {
-            catppuccin = {
-              enable = true;
-              flavor = "frappe";
-              accent = "blue";
-              ghostty.enable = false;
-            };
-          }
-          {
-            hm = {
-              atuin.enable = true;
-              bash.enable = true;
-              chromium.enable = true;
-              dconf.enable = true;
-              direnv.enable = true;
-              fastfetch.enable = true;
-              firefox.enable = true;
-              fzf.enable = true;
-              ghostty.enable = true;
-              git.enable = true;
-              helix.enable = true;
-              mpv.enable = true;
-              nixcord = {
-                enable = true;
-                theme = {
-                  dark = {
-                    inherit (config.catppuccin) flavor accent;
-                  };
-                  light = {
-                    flavor = "latte";
-                    inherit (config.catppuccin) accent;
-                  };
-                };
-              };
-              ssh.enable = true;
-              nushell.enable = true;
-              starship.enable = true;
-              vscode.enable = true;
-              yazi.enable = true;
-              zsh.enable = true;
-              zoxide.enable = true;
-            };
-            home.stateVersion = "25.05";
-          }
-        ];
-      };
     extraSpecialArgs = { inherit inputs; };
   };
+
+  home-manager.users.nyx =
+    { config, ... }:
+    {
+      imports =
+        let
+          modulesImports = [
+            ../../modules/hm/browser
+            ../../modules/hm/cli
+            ../../modules/hm/config/dconf.nix
+            ../../modules/hm/gui
+            ../../modules/hm/terminal
+            ../../modules/hm/tui
+            ../../shared/aliases.nix
+            { home.stateVersion = "25.05"; }
+          ];
+          catppuccin = [
+            inputs.catppuccin.homeModules.catppuccin
+            {
+              catppuccin = {
+                enable = true;
+                flavor = "frappe";
+                accent = "blue";
+                ghostty.enable = false;
+              };
+            }
+          ];
+          homePkgs =
+            let
+              catppuccin-userstyles = pkgs.callPackage ../../pkgs/catppuccin-userstyles.nix {
+                inherit (config.catppuccin) accent flavor;
+              };
+              warp-terminal-catppuccin = pkgs.callPackage ../../pkgs/warp-terminal-catppuccin.nix {
+                inherit (config.catppuccin) accent;
+              };
+            in
+            [
+              {
+                home = {
+                  file.".warp/themes".source = "${warp-terminal-catppuccin.outPath}/share/warp/themes";
+                  file."Documents/catppuccin-userstyles.json".source =
+                    "${catppuccin-userstyles.outPath}/dist/import.json";
+                };
+              }
+            ];
+          hm = [
+            {
+              hm = {
+                atuin.enable = true;
+                bash.enable = true;
+                chromium.enable = true;
+                direnv.enable = true;
+                fastfetch.enable = true;
+                firefox.enable = true;
+                fzf.enable = true;
+                ghostty.enable = true;
+                git.enable = true;
+                helix.enable = true;
+                mpv.enable = true;
+                nixcord.enable = true;
+                ssh.enable = true;
+                nushell.enable = true;
+                starship.enable = true;
+                vscode.enable = true;
+                yazi.enable = true;
+                zsh.enable = true;
+                zoxide.enable = true;
+              };
+            }
+          ];
+        in
+        modulesImports ++ catppuccin ++ homePkgs ++ hm;
+    };
 }
