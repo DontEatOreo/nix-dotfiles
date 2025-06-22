327548d42d04a52975e056d08253cc63d1172853
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-06-22 11:58:48 +0300

    stye(hosts/linux/default): use `[ ... ] ++ [ ... ]`

--- hosts/linux/default.nix
+++ hosts/linux/default.nix
@@ -1,37 +1,45 @@
 { inputs, ... }:
 {
   lenovo-legion = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
     specialArgs = { inherit inputs; };
-    modules = [
-      ./configuration.nix
-      ../../modules/common
-      ../../modules/nixos
-      {
-        nixOS = {
-          gnome.enable = true;
-        };
-      }
-
-      inputs.catppuccin.nixosModules.catppuccin
-      {
-        catppuccin = {
-          enable = true;
-          flavor = "frappe";
-          accent = "blue";
-        };
-      }
-      inputs.nur.modules.nixos.default
-      inputs.nixos-hardware.nixosModules.lenovo-legion-15arh05h
-      inputs.sops-nix.nixosModules.sops
-      {
-        sops = {
-          age.keyFile = "/home/nyx/.config/sops/age/keys.txt";
-          defaultSopsFile = ../../secrets/secrets.yaml;
-          secrets.github_ssh = { };
-          secrets.lenovo_legion_5_15arh05h_ssh = { };
-        };
-      }
-    ];
+    modules =
+      [
+        { nixpkgs.hostPlatform.system = "x86_64-linux"; }
+        ./configuration.nix
+      ]
+      ++ [
+        ../../modules/common
+        ../../modules/nixos
+        {
+          nixOS = {
+            gnome.enable = true;
+          };
+        }
+      ]
+      ++ [
+        inputs.catppuccin.nixosModules.catppuccin
+        {
+          catppuccin = {
+            enable = true;
+            flavor = "frappe";
+            accent = "blue";
+          };
+        }
+      ]
+      ++ [
+        inputs.sops-nix.nixosModules.sops
+        {
+          sops = {
+            age.keyFile = "/home/nyx/.config/sops/age/keys.txt";
+            defaultSopsFile = ../../secrets/secrets.yaml;
+            secrets.github_ssh = { };
+            secrets.lenovo_legion_5_15arh05h_ssh = { };
+          };
+        }
+      ]
+      ++ [
+        inputs.nur.modules.nixos.default
+        inputs.nixos-hardware.nixosModules.lenovo-legion-15arh05h
+      ];
   };
 }
