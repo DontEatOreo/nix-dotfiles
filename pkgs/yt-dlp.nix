49526664c84aab7224602198ced946a81156731f
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-07-08 23:27:34 +0300

    chore(yt-dlp): refactor, update

--- pkgs/yt-dlp.nix
+++ pkgs/yt-dlp.nix
@@ -6,6 +6,7 @@
   rtmpdump,
   atomicparsley,
   pandoc,
+  installShellFiles,
   atomicparsleySupport ? true,
   ffmpegSupport ? true,
   rtmpSupport ? true,
@@ -18,19 +19,22 @@ python3Packages.buildPythonApplication rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2025.06.25";
+  version = "2025.06.30-unstable-2025-07-08";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yt-dlp";
     repo = "yt-dlp";
-    tag = version;
-    hash = "sha256-ZEhruWP4RBeyw2vCF0CG7xvMur8RzVmTgi5Kz4nkNII=";
+    rev = "fd36b8f31bafbd8096bdb92a446a0c9c6081209c";
+    hash = "sha256-IpOfOQf9r5wCCwoYqGyOtJQAn/yvMJjL6F9jJ3x9IKw=";
   };
 
   build-system = with python3Packages; [ hatchling ];
 
-  nativeBuildInputs = [ pandoc ];
+  nativeBuildInputs = [
+    installShellFiles
+    pandoc
+  ];
 
   # expose optional-dependencies, but provide all features
   dependencies = lib.flatten (lib.attrValues optional-dependencies);
@@ -61,14 +65,12 @@ python3Packages.buildPythonApplication rec {
   postBuild = ''
     python devscripts/prepare_manpage.py yt-dlp.1.temp.md
     pandoc -s -f markdown-smart -t man yt-dlp.1.temp.md -o yt-dlp.1
-    rm -f yt-dlp.1.temp.md
+    rm yt-dlp.1.temp.md
 
-    mkdir -p completions/bash completions/zsh completions/fish
-    python devscripts/bash-completion.py
-    python devscripts/zsh-completion.py completions/zsh/_yt-dlp  
+    mkdir -p completions/{bash,fish,zsh}
+    python devscripts/bash-completion.py completions/bash/yt-dlp
+    python devscripts/zsh-completion.py completions/zsh/_yt-dlp
     python devscripts/fish-completion.py completions/fish/yt-dlp.fish
-
-    cp README.md README.txt
   '';
 
   # Ensure these utilities are available in $PATH:
@@ -90,28 +92,26 @@ python3Packages.buildPythonApplication rec {
   # Requires network
   doCheck = false;
 
-  postInstall = ''
-    install -Dm644 yt-dlp.1 $out/share/man/man1/yt-dlp.1
+  postInstall =
+    ''
+      installManPage yt-dlp.1
 
-    install -Dm644 completions/bash/yt-dlp $out/share/bash-completion/completions/yt-dlp
-    install -Dm644 completions/zsh/_yt-dlp $out/share/zsh/site-functions/_yt-dlp  
-    install -Dm644 completions/fish/yt-dlp.fish $out/share/fish/vendor_completions.d/yt-dlp.fish
+      installShellCompletion \
+        --bash completions/bash/yt-dlp \
+        --fish completions/fish/yt-dlp.fish \
+        --zsh completions/zsh/_yt-dlp
 
-    install -Dm644 README.txt $out/share/doc/yt-dlp/README.txt
-    install -Dm644 Changelog.md $out/share/doc/yt-dlp/Changelog.md
-
-    ${lib.optionalString withAlias ''
+      install -Dm644 Changelog.md README.md -t "$out/share/doc/yt-dlp"
+    ''
+    + lib.optionalString withAlias ''
       ln -s "$out/bin/yt-dlp" "$out/bin/youtube-dl"
-    ''}
-  '';
+    '';
 
-  passthru.updateScript = nix-update-script { };
-
-  meta = with lib; {
-    changelog = "https://github.com/yt-dlp/yt-dlp/blob/HEAD/Changelog.md";
+  meta = {
+    changelog = "https://github.com/yt-dlp/yt-dlp/blob/${version}/Changelog.md";
     description = "Command-line tool to download videos from YouTube.com and other sites (youtube-dl fork)";
     homepage = "https://github.com/yt-dlp/yt-dlp/";
-    license = licenses.unlicense;
+    license = lib.licenses.unlicense;
     longDescription = ''
       yt-dlp is a youtube-dl fork based on the now inactive youtube-dlc.
 
@@ -121,7 +121,7 @@ python3Packages.buildPythonApplication rec {
       you can modify it, redistribute it or use it however you like.
     '';
     mainProgram = "yt-dlp";
-    maintainers = with maintainers; [
+    maintainers = with lib.maintainers; [
       SuperSandro2000
       donteatoreo
     ];
