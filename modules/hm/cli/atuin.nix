a92fbd84d7610c69471a03e17c090c47941e18d6
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-08-03 01:35:08 +0300

    feat(hm/cli/atuin): use `pkgsUnstable`

--- modules/hm/cli/atuin.nix
+++ modules/hm/cli/atuin.nix
@@ -1,10 +1,16 @@
-{ lib, config, ... }:
+{
+  pkgsUnstable,
+  lib,
+  config,
+  ...
+}:
 {
   options.hm.atuin.enable = lib.mkEnableOption "Atuin";
 
   config = lib.mkIf config.hm.atuin.enable {
     programs.atuin = {
       enable = true;
+      package = pkgsUnstable.atuin;
       daemon.enable = true;
       flags = [ "--disable-up-arrow" ];
     };
